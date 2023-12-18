3aa5a1c15df8186c1935dc414b2f7a4ee7d900c5
noname068218 <128069790+noname068218@users.noreply.github.com>
2023-12-18 18:33:00 +0100

    completed hero section add: books and styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,35 @@
 import { useState } from "react";
-import book1 from "../../assets/book/book1.jpg";
-import book2 from "../../assets/book/book2.jpg";
+import book2 from "../../assets/book/book1.jpg";
+import book1 from "../../assets/book/book2.jpg";
 import book3 from "../../assets/book/book3.jpg";
 import Vector from "../../assets/website/blue-pattern.png";
+const ImageList = [
+  {
+    id: 1,
+    img: book1,
+    title: "His Life will forever be Changed",
+    description:
+      "lorem His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 2,
+    img: book2,
+    title: "Who's there",
+    description:
+      "Who's there lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 3,
+    img: book3,
+    title: "Lost Boy",
+    description:
+      "Lost Boy, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
 
 const Hero = () => {
-  const [imageid, setImageId] = useState(book2);
-  const [title, setTitle] = useState("his Life will forever be Changed");
+  const [imageid, setImageId] = useState(book1);
+  const [title, setTitle] = useState("His Life will forever be Changed");
   const [description, setDescriprion] = useState(
     "lorem His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
   );
@@ -53,6 +76,24 @@ const Hero = () => {
                 />
               </div>
               {/* other Image list*/}
+
+              <div className="flex lg:flex-col lg:top-1/2 lg:-translate-y-1/2 lg:py-2 justify-center gap-4 absolute -bottom-[40px] lg:-right-1 bg-white rounded-full">
+                {ImageList.map((item) => (
+                  <img
+                    key={item.id}
+                    src={item.img}
+                    alt="Top Books"
+                    className="max-w-[100px] h-[100px] object-contain inline-block hover:scale-110 duration-200 ease-in"
+                    onClick={() => {
+                      setImageId(
+                        item.id === 1 ? book1 : item.id === 2 ? book2 : book3
+                      );
+                      setTitle(item.title);
+                      setDescriprion(item.description);
+                    }}
+                  />
+                ))}
+              </div>
             </div>
           </div>
         </div>
