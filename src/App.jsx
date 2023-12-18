beaae999401979db6c732e89e8791b82ebe9d499
noname068218 <128069790+noname068218@users.noreply.github.com>
2023-12-18 22:39:27 +0100

    Library Section end

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import BestBook from "./components/Services/Services";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Banner from "./components/Banner/Banner";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <BestBook />
+        <Banner />
       </div>
     </>
   );
